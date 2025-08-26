b49c103a22630b48b632b6421fb3aa9139d73c90
ASHIS KUMAR SWAIN <khitishkumarswain@Ashis-Mac-M1.local>
2025-08-27 01:05:03 +0530

    name
    name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,14 @@ const exo2 = Exo_2({
 })
 
 export const metadata: Metadata = {
-  title: "Kinh Bach | Frontend Developer Portfolio",
+  title: "Ashis Kumar Swain | Frontend Developer Portfolio",
   description:
-    "Explore Kinh Bach's portfolio – a skilled Frontend Developer specializing in Next.js, React, and modern web technologies. Showcasing innovative projects, UI/UX expertise, and high-performance web applications.",
+    "Explore Ashis Kumar Swain's portfolio – a skilled Frontend Developer specializing in Next.js, React, and modern web technologies. Showcasing innovative projects, UI/UX expertise, and high-performance web applications.",
   keywords: [
     "Frontend Developer",
     "Next.js Developer",
     "React Developer",
-    "Kinh Bach Portfolio",
+    "Ashis Kumar Swain Portfolio",
     "Web Development",
     "UI/UX Design",
     "JavaScript",
@@ -29,9 +29,9 @@ export const metadata: Metadata = {
     "Modern Web Technologies",
   ],
   openGraph: {
-    title: "Kinh Bach | Frontend Developer Portfolio",
+    title: "Ashis Kumar Swain | Frontend Developer Portfolio",
     description:
-      "Discover Kinh Bach's expertise in Frontend Development, React, and Next.js. View projects, case studies, and technical skills.",
+      "Discover Ashis Kumar Swain's expertise in Frontend Development, React, and Next.js. View projects, case studies, and technical skills.",
     url: "https://kinhdev.id.vn",
     type: "website",
     images: [
@@ -39,15 +39,15 @@ export const metadata: Metadata = {
         url: "https://voocgavdbpy2gucg.public.blob.vercel-storage.com/open-graph-6fkPvt3jl60AhDWy2pPhfp3PKoZPrZ.png",
         width: 1200,
         height: 630,
-        alt: "Kinh Bach - Frontend Developer Portfolio",
+        alt: "Ashis Kumar Swain - Frontend Developer Portfolio",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Kinh Bach | Frontend Developer Portfolio",
+    title: "Ashis Kumar Swain | Frontend Developer Portfolio",
     description:
-      "Explore Kinh Bach's frontend projects and expertise in React, Next.js, and modern web development.",
+      "Explore Ashis Kumar Swain's frontend projects and expertise in React, Next.js, and modern web development.",
     images: [
       "https://voocgavdbpy2gucg.public.blob.vercel-storage.com/open-graph-6fkPvt3jl60AhDWy2pPhfp3PKoZPrZ.png",
     ],
